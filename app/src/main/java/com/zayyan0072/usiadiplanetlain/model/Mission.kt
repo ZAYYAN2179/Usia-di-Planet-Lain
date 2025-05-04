9b918196799310728da805ad0c60b4656e2dbd9e
Muh Zayyan Al Thaaf Nur <aatzayyan49@gmail.com>
2025-05-04 22:08:41 +0700

    Membuat Model Mission

---
+++ app/src/main/java/com/zayyan0072/usiadiplanetlain/model/Mission.kt
@@ -0,0 +1,10 @@
+package com.zayyan0072.usiadiplanetlain.model
+
+data class Mission(
+    val namaPlanet: String,
+    val tanggalMisi: String,
+    val tipeMisi: String,
+    val penemuan: String,
+    val tantangan: String,
+    val insightPenjelajahan: String
+)
